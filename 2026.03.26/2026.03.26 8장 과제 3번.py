3772124ee7a81d7fa2db73c8d7e38843e7f24fd8
kimjisu22 <jiddu0442@gmail.com>
2026-03-27 19:01:17 +0900

    03.27 저장은 되나 조회하면 초기화 되는걸 ㅠㅠㅠㅠㅠㅠㅠㅠㅠ

--- 2026.03.26/2026.03.26 8장 과제 3번.py
+++ 2026.03.26/2026.03.26 8장 과제 3번.py
@@ -4,26 +4,31 @@
 tel = {'철수': '010-1111-1111', '영희': '010-2222-2222', '민수': '010-3333-3333', '지혜':'010-4444-4444' }
 
 add = int(input("1.연락처 추가\n2.연락처 조회\n3.종료\n메뉴를 선택하세요: "))
+i = 1
 
+while i < len(tel):
 
-if add == 1:
-    name = input("이름을 입력하세요: ")
-    phone = input("전화번호를 입력하세요: ")
-    tel[name] = phone
+    if add == 1:
+        name = input("이름을 입력하세요: ")
+        phone = input("전화번호를 입력하세요: ")
+        tel[name] = phone
+    
+        print(f"{name}의 연락처가 저장되었습니다.")
+        break
+        
+    elif add == 2:
+        select = input("조회할 이름을 입력하세요: ")
+        print(f"{select}의 전화번호: ",) 
+        break
 
-    print(f"{name}의 연락처가 저장되었습니다.")
-
-elif add == 2:
-    select = input("조회할 이름을 입력하세요: ")
-    print(f"{select}의 전화번호: ",) 
-
-elif add == 3:
-    print("프로그램을 종료합니다")
-
+    elif add == 3:
+        print("프로그램을 종료합니다")
+    i+=1    
 else:
     print("올바른 번호를 입력하세요")
     
-
+    
+        
 
 
 
